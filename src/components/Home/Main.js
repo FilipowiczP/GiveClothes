f4ca14ebcacf24d474b0dc35ddb9b1acaa75be94
FilipowiczP <przemyslaw.filipowicz1@gmail.com>
2020-09-02 19:29:16 +0200

    Created Home-> Main section

--- src/components/Home/Main.js
+++ src/components/Home/Main.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import homeImg from '../images/home.png'
+import homeImg from '../images/home.png';
+import decoration from '../images/Decoration.svg';
 import {Link} from 'react-scroll';
 import './scss/main.scss';
 
@@ -7,7 +8,7 @@ const Main = () => {
     return(
         <main className="main" id="main">
             <div className="main__box">
-                <img src={homeImg} className="main__box__image" />
+                <img src={homeImg} alt="foto" className="main__box__image" />
             </div>
             <div>
                 <header className="main__header">
@@ -24,6 +25,15 @@ const Main = () => {
                         </ul>
                     </nav>
                 </header>
+                <div className="main__content">
+                    <h1 className="main__content__text">Zacznij pomagać</h1>
+                    <h1 className="main__content__text">Oddaj niechciane rzeczy w zaufane ręce</h1>
+                    <img src={decoration} alt="decoration" />
+                    <div className="main__content__box">
+                        <Link className="main__content__box__link"  activeClass="active" to="" smooth={true} offset={0} duration={500}>Oddaj rzeczy</Link>
+                        <Link className="main__content__box__link"  activeClass="active" to="" smooth={true} offset={0} duration={500}>Zorganizuj zbiórkę</Link>
+                    </div>
+                </div>
             </div>
         </main>
     );
