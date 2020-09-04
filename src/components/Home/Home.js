cbc9ea0847c3a9293133f5277b014efdfb29ab75
FilipowiczP <przemyslaw.filipowicz1@gmail.com>
2020-09-04 10:56:38 +0200

    Created second section

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Main from './Main';
 import FirstSection from './FirstSection';
+import SecondSection from './SecondSection';
 
 const Home = () => {
     return(
         <>
             <Main />
             <FirstSection />
+            <SecondSection />
         </>
     );
 }
---
+++ src/components/Home/SecondSection.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import './scss/secondSection.scss';
+import decoration from '../images/Decoration.svg';
+import icon1 from '../images/Icon-1.svg'
+import icon2 from '../images/Icon-2.svg'
+import icon3 from '../images/Icon-3.svg'
+import icon4 from '../images/Icon-4.svg'
+import { Link } from 'react-scroll';
+
+const SecondSection = () => {
+    return(
+        <section className="secondSection" id="secondSection">
+            <h1 className="secondSection__title">Wystarczą 4 proste kroki</h1>
+            <img src={decoration} alt="decoration" className="secondSection__img" />
+            
+            <div className="secondSection__content">
+                <div className="secondSection__content__box">
+                    <img src={icon1} alt="icon" />
+                    <h2 className="secondSection__content__box__title">Wybierz rzeczy</h2>
+                    <p>ubrania, zabawki, sprzęt i inne</p>
+                </div>
+                <div className="secondSection__content__box">
+                    <img src={icon2} alt="icon" />
+                    <h2 className="secondSection__content__box__title">Spakuj je</h2>
+                    <p>skorzystaj z worków na śmieci</p>
+                </div>
+                <div className="secondSection__content__box">
+                    <img src={icon3} alt="icon" />
+                    <h2 className="secondSection__content__box__title">Zdecyduj komu chcesz pomóc</h2>
+                    <p>wybierz zaufane miejsce</p>
+                </div>
+                <div className="secondSection__content__box">
+                    <img src={icon4} alt="icon" />
+                    <h2 className="secondSection__content__box__title">Zamów kuriera</h2>
+                    <p>kurier przyjedzie w dogodnym terminie</p>
+                </div>
+            </div>
+
+            <Link className="secondSection__link"  activeClass="active" to="" smooth={true} offset={0} duration={500}>Oddaj rzeczy</Link>
+
+        </section>
+    );
+}
+
+export default SecondSection;
