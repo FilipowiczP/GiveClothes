f220766f9d777f1dd2a9d78ced53b7503aa728b8
FilipowiczP <przemyslaw.filipowicz1@gmail.com>
2020-09-14 18:05:24 +0200

    Created validation

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -11,19 +11,36 @@ const Register = () =>{
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [repeatPassword, setRepeatPassword] = useState('');
+
+    const [errorEmail, setErrorEmail] = useState(false);
+    const [errorPassword, setErrorPassword] = useState(false);
 
     const onSubmit = (event) =>{
         event.preventDefault();
+        
+        if(email.length < 15){
 
-        firebase.firestore().collection('profile').add({
-            email,
-            password
-        })
-        .then(()=>{
-            setEmail('')
-            setPassword('')
-        })
+            setErrorEmail(true);
+
+        }else if(password !== repeatPassword){
+
+            setErrorPassword(true);
+
+        }else{
+            setErrorPassword(false);
+            firebase.firestore().collection('profile').add({
+                email,
+                password
+            })
+            .then(()=>{
+                setEmail('');
+                setPassword('');
+                setRepeatPassword('');
+            })
+        }
     }
+    
     return(
         <>
             <Navigation />
@@ -31,20 +48,24 @@ const Register = () =>{
                 <h1 className="register__title">Załóż kotno</h1>
                 <img src={decoration} alt="decoration" className="register__decoration" />
                 <form className="register__form" onSubmit={onSubmit}>
+                    <p className={errorEmail ? "register__form__validation-error" : "register__form__validation"}>Nie poprawny email</p>
+                   
                     <label for="email" className="register__form__label">Email</label>
-                    <input type="email" id="email" className="register__form__input" value={email} onChange={e => setEmail(e.currentTarget.value)}/>
+                    <input type="email" id="email" className="register__form__input" value={email} onChange={e => setEmail(e.currentTarget.value)}/> 
+
+                    <p className={errorPassword ? "register__form__validation-error" : "register__form__validation"}>Nie poprawne hasło</p>
                     
                     <label for="password" className="register__form__label">Hasło</label>
                     <input type="password" id="password" className="register__form__input" value={password} onChange={e => setPassword(e.currentTarget.value)} />
                    
                     <label for="repeatPassword" className="register__form__label">Powtórz hasło</label>
-                    <input type="password" id="repeatPassword" className="register__form__input" />
+                    <input type="password" id="repeatPassword" className="register__form__input" value={repeatPassword} onChange={e => setRepeatPassword(e.currentTarget.value)} />
+                   
                     <button type="submit">Załóż konto</button>
                 </form>
-                <div className="register__box">
-                    <Link to="/login" className="register__box__link">Zaloguj</Link>
-                    {/* <Link to="/register" className="register__box__link">Załóż konto</Link> */}
-                </div>
+
+                <Link to="/login" className="register__link">Zaloguj</Link>
+
             </main>
         </>
     );
